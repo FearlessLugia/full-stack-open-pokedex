9d3c886d4e4294baccc40a00082d98cc8a86e82b
FearlessLugia <ajxx030217@163.com>
2023-08-31 19:43:05 +0800

    e2e test for individual pokemon

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -1,13 +1,19 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
+    // eslint-disable-next-line no-undef
     cy.visit('http://localhost:5000')
+    // eslint-disable-next-line no-undef
     cy.contains('ivysaur')
+    // eslint-disable-next-line no-undef
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
   it('can navigate from the main page to the page of a particular Pokemon',function(){
+    // eslint-disable-next-line no-undef
     cy.visit('http://localhost:5000')
+    // eslint-disable-next-line no-undef
     cy.contains('ivysaur').click()
+    // eslint-disable-next-line no-undef
     cy.contains('chlorophyll')
   })
 })
